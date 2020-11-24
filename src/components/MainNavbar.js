75fb7881cb5d8ed7f324b468bb9c20c9b82d4b3f
Hoi Nguyen <vuhoinguyen@gmail.com>
2020-11-25 00:17:51 +0100

    Change Nav Link href's

--- src/components/MainNavbar.js
+++ src/components/MainNavbar.js
@@ -6,13 +6,13 @@ const MainNavbar=() => {
     return (
         <Navbar className="navbar" bg="dark" expand="lg" sticky="top" variant="dark">
             <Navbar.Brand href="#home">NGUYEN HIEP</Navbar.Brand>
-            <Navbar.Collapse id="collapse-navbar-nav">
+            <Navbar.Collapse id="navbar-collapse">
                 <Nav className="navbar-nav justify-content-end">
                     <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#link">Paintings</Nav.Link>
-                    <Nav.Link href="#link">Sculptures</Nav.Link>
-                    <Nav.Link href="#link">Books</Nav.Link>
-                    <Nav.Link href="#link">Contact</Nav.Link>
+                    <Nav.Link href="#paintings">Paintings</Nav.Link>
+                    <Nav.Link href="#sculptures">Sculptures</Nav.Link>
+                    <Nav.Link href="#books">Books</Nav.Link>
+                    <Nav.Link href="#contact">Contact</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
             <Navbar.Toggle aria-controls="collapse-navbar-nav" />
